3533c0fef524008373d43c1563610615ad3098b4
hphp <hphpcarrot@gmail.com>
2013-11-05 22:05:56 +0800

    argu,split,write_tofile,listfile

---
+++ demo_python/d_argu.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import os
+import sys
+
+for arg in sys.argv:
+    print arg
+print sys.argv[1]
+print sys.argv[2]
---
+++ demo_python/d_write_file.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+import csv
+csv_writer = csv.writer(open(filename, "a"),delimiter=",") # write append.
