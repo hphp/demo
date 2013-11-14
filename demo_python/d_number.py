9ea6262020096ef5beb32515d122d969ee01d694
hphp <hphpcarrot@gmail.com>
2013-11-14 19:15:45 +0800

    number character

---
+++ demo_python/d_number.py
@@ -0,0 +1,19 @@
+
+def transfer():
+    a = 5.5
+    b = 5.8
+    d = 5.2
+    c = int(a)
+    print c
+    c = int(b)
+    print c
+    c = int(d)
+    print c
+
+def divide():
+    a = 100
+    b = 3
+    print a/b
+#transfer()
+divide()
+
