1d1c9fc43aaceeeb6f9f86d50f163fe4cc43ac4b
hphp <hphpcarrot@gmail.com>
2013-11-15 22:50:37 +0800

    sklearn_svm,

--- demo_python/demo_scikit_learn/d_svm.py
+++ demo_python/demo_scikit_learn/d_svm.py
@@ -14,4 +14,24 @@ def input_type():
     y = [1, 1, 0]
     print type(X),type(y),len(X),len(y) 
 
-input_type()
+def if_could_get_trained_continuously():
+    X = [[0, 0], [1, 1] , [-1,-1]]
+    y = [1, 1, 0]
+    clf = svm.SVC()
+    clf.fit(X, y) 
+    print clf.predict([2,2])
+    X = [[3,3]]
+    y = [0]
+    clf.fit(X, y) 
+    print clf.predict([2,2])
+    X = [[4,4]]
+    y = [0]
+    clf.fit(X, y) 
+    print clf.predict([2,2])
+    X = [[2,2]]
+    y = [0]
+    clf.fit(X, y) 
+    print clf.predict([2,2])
+
+if_could_get_trained_continiously()    
+#input_type()
