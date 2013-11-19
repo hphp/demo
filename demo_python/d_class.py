650c8d09e8952f6aa1ed52eb5ebd37c91e8b026b
hphp <hphpcarrot@gmail.com>
2013-11-19 22:41:26 +0800

    1.global,global.class;\n;2.class;\n;

---
+++ demo_python/d_class.py
@@ -0,0 +1,6 @@
+
+class d_TEMPLATE():
+    def __init__(self,v=0,m=2,n=150):
+        self.v = v 
+        self.m = m
+        self.n = n
---
+++ demo_python/d_global.py
@@ -0,0 +1,24 @@
+
+from d_class import d_TEMPLATE
+
+a = 4
+b = int()
+c = d_TEMPLATE()
+
+def test_class():
+    global c
+    print c.m
+    c = d_TEMPLATE(2,3,4)
+    print c.m
+
+def test_a():
+    global a, b
+    print a, b
+    #global a, b
+    print a, b
+    a += 4
+    b = 4
+
+test_class()
+#test_a()
+#print a, b
