5bf24e810c43f3c811c8db634c4651a63444e7d4
hphp <hphpcarrot@gmail.com>
2013-11-20 14:41:45 +0800

    1.img.show() using pylab;\n;2.array2PIL,PIL2array;\n;3.random_pixel_img

--- python/d_img.py
+++ python/d_img.py
@@ -2,6 +2,39 @@
 
 from PIL import Image,ImageEnhance,ImageFilter
 import numpy
+import cv2
+import pylab
+
+def random_pixel_img():
+    rng = numpy.random.RandomState(22222)
+    rand_l = rng.uniform(0, 255, size=(50, 50))
+    mode = 'RGBA'
+    size = (50, 50)
+    n_img = Image.frombuffer(mode, size, rand_l.tostring(), 'raw', mode, 0, 1)
+    pylab.axis('off'); pylab.imshow(n_img)
+    pylab.show()
+
+def show_img():
+    img = Image.open('cat.5123.jpg')
+    pylab.axis('off'); pylab.imshow(img)
+    pylab.show()
+
+def PIL2array():
+    img = Image.open('cat.5123.jpg')
+    pylab.axis('off'); pylab.imshow(img)
+    pylab.show()
+    return (numpy.array(img.getdata(), numpy.uint8).reshape(img.size[1], img.size[0], 3), img.size)
+
+def array2PIL():
+    arr, size = PIL2array()
+    print size
+    mode = 'RGBA'
+    arr = arr.reshape(arr.shape[0]*arr.shape[1], arr.shape[2])
+    if len(arr[0]) == 3:
+        arr = numpy.c_[arr, 255*numpy.ones((len(arr),1), numpy.uint8)]
+    n_img = Image.frombuffer(mode, size, arr.tostring(), 'raw', mode, 0, 1)
+    pylab.axis('off'); pylab.imshow(n_img)
+    pylab.show()
 
 def load_img_to_list():
     image_name="cat.5123.jpg"
@@ -31,4 +64,7 @@ def test_hw_order():
                     count += 1
     print count
 #load_img_to_list()
-test_hw_order()
+#test_hw_order()
+#array2PIL()
+#show_img()
+random_pixel_img()
