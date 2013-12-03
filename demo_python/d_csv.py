58fca4e4c8a529d2b5a20ce3ea553bbc5b16f458
hphp <hphpcarrot@gmail.com>
2013-12-04 01:31:25 +0800

    modify

--- demo_python/d_csv.py
+++ demo_python/d_csv.py
@@ -1,20 +1,25 @@
 
 import csv
-import numpy
+import numpy as np
 
 def basic(filname):
     csv_reader = csv.reader(open(filname, "r"), delimiter=",")
-    print type(csv_reader)
+    print type(csv_reader) # a csv reader instance , no len() function.
 
 def read_data_patch_to_ndarray(filname="../data/train.csv", start_line=0, limit=None):
 
-    print "Reading data %s of line[%d,%d)" % (filname, start_line, start_line + limit)
+    print "Reading data %s of lines %s from starting line %d" % (filname, str(limit), start_line)
     data = []
     labels = []
     csv_reader = csv.reader(open(filname, "r"), delimiter=",")
-    index = 0 
-    for index in range(start_line, start_line+limit):
-        row = csv_reader[index]
+    index = -1 
+    for row in csv_reader:
+        index += 1
+        if index < start_line:
+            continue
+        if (limit != None) :
+            if (index >= (start_line+limit)):
+                break
         labels.append(int(row[0]))
         row = row[1:]
         data.append(np.float32(row)/255)
@@ -23,4 +28,9 @@ def read_data_patch_to_ndarray(filname="../data/train.csv", start_line=0, limit=
     data_y=np.asarray(labels,dtype=np.int32)
     return (data_x, data_y)
 
-basic("dataset.expl")
+#basic("dataset.expl")
+dataset = read_data_patch_to_ndarray("dataset.expl",1,4)
+#dataset = read_data_patch_to_ndarray("dataset.expl",1)
+#dataset = read_data_patch_to_ndarray("dataset.expl")
+print dataset[0].shape, dataset[1].shape
+print dataset
--- demo_python/spams_python/example.py
+++ demo_python/spams_python/example.py
@@ -2,4 +2,5 @@ import numpy as np
 import spams
 X = np.asfortranarray(np.random.random((64,200)))
 Y = np.asfortranarray(np.random.random((200,20000)))
-#Z = spams.CalcXY(X,Y)
+Z = spams.calcXY(X,Y)
+print Z.shape
