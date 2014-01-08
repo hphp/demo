2e5d74994dd92cface868ac8186391e383597624
hphp <hphpcarrot@gmail.com>
2014-01-08 16:40:38 +0800

    demo_python/d_str.py 'hello%08dworld'%4

--- demo_python/d_str.py
+++ demo_python/d_str.py
@@ -1,9 +1,16 @@
 
-a = "abcd"
-b = "0123"
-c = "!@#$"
+def basic():
+    a = "abcd"
+    b = "0123"
+    c = "!@#$"
 
-sep = "."
-print sep.join((a,b,c)) #a.b.c
-print sep.join([a,b,c]) #a.b.c 
-print " ".join([a,b,c])
+    sep = "."
+    print sep.join((a,b,c)) #a.b.c
+    print sep.join([a,b,c]) #a.b.c 
+    print " ".join([a,b,c])
+
+def strange():
+    ns = "hellomorto-%08d!!"%(3+1)
+    print ns
+
+strange()
