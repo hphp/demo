9a2c453f2abffea3c4f9c29266dcc2898d9e8ad8
hphp <hphpcarrot@gmail.com>
2014-01-09 18:45:17 +0800

    demo_python/d_str.py ascii_to_char

--- demo_python/d_str.py
+++ demo_python/d_str.py
@@ -1,4 +1,14 @@
 
+def ascii_to_char():
+    new_char = "%c"%2
+    print new_char
+    new_char = "%c"%182
+    print new_char
+    new_char = "%c"%33
+    print new_char
+    new_char = "%c"%174
+    print new_char
+
 def basic():
     a = "abcd"
     b = "0123"
@@ -13,4 +23,14 @@ def strange():
     ns = "hellomorto-%08d!!"%(3+1)
     print ns
 
-strange()
+def rep():
+    ostr = "hello world i love u "
+    # nstr = ostr.replace(" ",some-special-char)  could not right now , encodes or sth
+    new_char = "%c"%216
+    nstr = ostr.replace(" ",new_char)
+    print ostr, nstr
+
+
+#strange()
+rep()
+#ascii_to_char()
