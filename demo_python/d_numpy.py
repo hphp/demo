3c8f460fbafab715e26b8c6e35ddc342bbb775a4
hphp <hphpcarrot@gmail.com>
2014-01-10 16:41:58 +0800

    demo_python/d_numpy.py where(), arange(), amax()

--- demo_python/d_numpy.py
+++ demo_python/d_numpy.py
@@ -2,6 +2,48 @@
 import numpy
 import random
 
+def f_amax():
+    l = numpy.arange(10)
+    print numpy.amax(l)
+
+def f_arange():
+    x = numpy.arange(9.).reshape(3,3)
+    print x
+    x = numpy.arange(9).reshape(3,3)
+    print x
+
+def f_where():
+    # where( condition ) , return position that satisfy condition
+    l = numpy.arange(10)
+    l2 = numpy.where((l>3)&(l<7))
+    print l2 # [ 4,5,6 ]
+    l3 = (l>3)&(l<7)
+    print l3 # [ False,....True,True, .. False ]
+    l4 = numpy.where(l3)
+    print l4 # [ 4,5,6 ]
+
+    # prove that it returns the postion instead of the element value
+    l = numpy.arange(10,20)
+    l2 = numpy.where((l>13)&(l<17))
+    print l2 # [ 4,5,6 ]
+    l3 = (l>13)&(l<17)
+    print l3 # [ False,....True,True, .. False ]
+    l4 = numpy.where(l3)
+    print l4 # [ 4,5,6 ]
+    #uni_l = rng.uniform(0, 255, size=(2500,))
+
+    #
+    x = numpy.arange(10,19).reshape(3,3)
+    print x
+    print numpy.where( x > 15 )
+    print numpy.where( x > 13 )
+    print numpy.where( x>15, x, -1 )
+
+    x = numpy.arange(10,22).reshape(3,4)
+    print x
+    print numpy.where( x > 15 )
+    print numpy.where( x > 13 )
+
 def uniform():
     rng = numpy.random.RandomState(23555)
     uni_l = rng.uniform(0, 255, size=(2500,))
@@ -27,4 +69,7 @@ def basic():
         print 'c between [ %d, %d )' % (s,e)
 
 #basic()
-uniform()
+#uniform()
+#f_where()
+#f_arange()
+f_amax()
