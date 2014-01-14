67a8a17d4469582cabe9d2dd1369d5f60b0b8b85
hphp <hphpcarrot@gmail.com>
2014-01-14 21:55:58 +0800

    demo_python/d_split_str.py split results of multiple seperators

--- demo_python/d_split_str.py
+++ demo_python/d_split_str.py
@@ -1,5 +1,13 @@
 
-s = "cat.5.jpg"
-slist = s.split(".")
-for i in slist:
-    print i
+def basic():
+    s = "cat.5.jpg"
+    slist = s.split(".")
+    for i in slist:
+        print i
+
+def multi_char():
+    s = "cat..5.jpg..kk."
+    for i in s.split("."):
+        print '-'+i+'-'
+
+multi_char()
