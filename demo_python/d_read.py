ad6f442627c829d0bdab199f303e08a7a545c597
hphp <hphpcarrot@gmail.com>
2014-01-14 11:15:28 +0800

    tmp

--- demo_python/d_read.py
+++ demo_python/d_read.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-content = "ff"
-f = open(content,'r+')
-s = "hello"
-f.write(s)
-print f
+def basic():
+    content = "ff"
+    f = open(content,'r+')
+    s = "hello"
+    f.write(s)
+    print f
 
-print f.readlines()
+    print f.readlines()
 
