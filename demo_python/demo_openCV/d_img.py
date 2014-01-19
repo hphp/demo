3e7206f948edbd6489e647260f3ab8840024176d
hphp <hphpcarrot@gmail.com>
2014-01-19 18:53:35 +0800

    demo_python/demo_openCV/d_img.py cv.SaveImage(), cv.LoadImage()

--- demo_python/demo_openCV/d_img.py
+++ demo_python/demo_openCV/d_img.py
@@ -1,6 +1,13 @@
 
 import cv
 import cv2
-   
-img = cv2.imread('cat.5123.jpg')
-cv2.imwrite('cat.cp.jpg', img)
+
+def cv2_img():
+    img = cv2.imread('cat.5123.jpg')
+    cv2.imwrite('cat.cp.jpg', img)
+
+def cv_img():
+    img = cv.LoadImage('cat.5123.jpg',0)
+    cv.SaveImage('cat.cp.jpg', img)
+
+cv_img()
