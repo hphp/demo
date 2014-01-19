da89cb960dd9c401314818b73e7a40b52985900a
hphp <hphpcarrot@gmail.com>
2014-01-19 17:39:16 +0800

    tmp

--- demo_python/d_argu.py
+++ demo_python/d_argu.py
@@ -2,7 +2,11 @@
 import os
 import sys
 
-for arg in sys.argv:
-    print arg
-print sys.argv[1]
-print sys.argv[2]
+def basic():
+    print len(sys.argv)
+    for arg in sys.argv:
+        print arg
+    print sys.argv[1]
+    print sys.argv[2]
+
+basic()
