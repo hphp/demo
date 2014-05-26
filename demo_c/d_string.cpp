a4303777cd77d99735e4419d7c55b676984e4fd7
hphp <hphpcarrot@gmail.com>
2014-05-26 16:13:57 +0800

    demo_c/d_string.cpp

---
+++ demo_c/d_string.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a = "abcd";
+    //printf("%s",a);
+    /*
+    cout << a;
+    cout << a.length();
+    */
+    string b = "abd";
+    printf("%d",a<b);
+    return 0;
+}
