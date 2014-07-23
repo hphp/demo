58b48c078c4acef7fb67b6f2448d473cbbfdcdd3
hphp <hphpcarrot@gmail.com>
2014-07-23 10:24:44 +0800

    demo_python/print raw %r

--- demo_python/d_print.py
+++ demo_python/d_print.py
@@ -1,9 +1,19 @@
 
-backslash = "\\"
-print backslash
-backslash = r'\adfsfd\dfdsfsd'
-print backslash
-backslash = r''
-lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds']
-backslash = ".".join(lst)
-print backslash
+def basic():
+    backslash = "\\"
+    print backslash
+    backslash = r'\adfsfd\dfdsfsd'
+    print backslash
+
+def join():
+    backslash = r''
+    lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds']
+    backslash = ".".join(lst)
+    print backslash
+
+def print_raw():
+    formatstr = "%r %r %r %r"
+    lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds', '\"hello morto\"']
+    print formatstr % (lst[0], lst[1], lst[2], lst[3])
+
+print_raw()
