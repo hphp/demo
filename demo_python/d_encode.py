5996e423a349e077bbb7cf89fd30919e34bd7d6f
hphp <hphpcarrot@gmail.com>
2014-07-23 17:00:02 +0800

    demo_python/中文encoding

--- demo_python/d_encode.py
+++ demo_python/d_encode.py
@@ -1,9 +1,17 @@
 #!/bin/python
+#encoding:utf-8  
 
 from urllib import urlencode
 import urllib
 
-#m = {'name':'peter','gender':'fe'}
-m = '/\good'
-#print urlencode(m)
-print urllib.quote(m)
+def d_urlencode():
+    #m = {'name':'peter','gender':'fe'}
+    m = '/\good'
+    #print urlencode(m)
+    print urllib.quote(m)
+
+def d_zhongwen_encoding():
+    #please see as encoding:utf-8
+    print "hello morto 你好！大魔tua！～"
+
+#d_zhongwen_encoding()
--- demo_python/d_print.py
+++ demo_python/d_print.py
@@ -1,19 +1,19 @@
 
-def basic():
+def basic_backslash():
     backslash = "\\"
     print backslash
     backslash = r'\adfsfd\dfdsfsd'
     print backslash
 
-def join():
+def join_backslash():
     backslash = r''
-    lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds']
+    lst = ['dsfds',r'\"sdfds\"','\"dfsfds\"dsfds']
     backslash = ".".join(lst)
     print backslash
 
 def print_raw():
     formatstr = "%r %r %r %r"
-    lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds', '\"hello morto\"']
+    lst = ['\\"dsfds\\"',r'\"sdfds\"','"dfsfds"dsfds', '\"hello morto\"']
     print formatstr % (lst[0], lst[1], lst[2], lst[3])
 
 print_raw()
