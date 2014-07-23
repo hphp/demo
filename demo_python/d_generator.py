64fb8bfc84ca6e6f77a79996d603ea497ef5b2e6
hphp <hphpcarrot@gmail.com>
2014-07-23 20:27:29 +0800

    demo_python/generator iterator to list

--- demo_python/d_generator.py
+++ demo_python/d_generator.py
@@ -1,6 +1,18 @@
 
-l = (i for i in xrange(100) if i & 1)
+l = (i for i in xrange(100) if i > 50)
 print l
-print len(list(l))
+lst = list(l)
+print len(lst)
+print len(lst)
+print len(lst)
+
+
+'''
+for ele in lst:
+    print ele
+print 'hello'
+for ele in lst:
+    print ele
 #for ele in l:
 #    print ele
+'''
