8aa357542ba52e8166f607565e0db67f3f291684
hphp <hphpcarrot@gmail.com>
2014-07-23 10:19:10 +0800

    demo_python/print backslash

---
+++ demo_python/d_print.py
@@ -0,0 +1,9 @@
+
+backslash = "\\"
+print backslash
+backslash = r'\adfsfd\dfdsfsd'
+print backslash
+backslash = r''
+lst = ['dsfds',r'\"sdfds\"','"dfsfds"dsfds']
+backslash = ".".join(lst)
+print backslash
