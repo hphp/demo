9624cad76a810ce42f3284490460e9f6f5261e56
hphp <hphpcarrot@gmail.com>
2014-07-30 20:41:30 +0800

    python/urllib

--- demo_python/d_urllib.py
+++ demo_python/d_urllib.py
@@ -19,6 +19,28 @@ def get_header_test():
     print msg.getparam("ETag") # dontknow what it is
     print '---'
 
+def get_header_list():
+    return [['Reference', 'baidu.com']]
+
+def set_header(request_handle, header_list):
+    for item in header_list:
+        request_handle.add_header(item[0], item[1])
+
+    return
+
+def test():
+    url=""
+    full_url='http://www.baidu.com'+url
+    request_handle = urllib2.Request(full_url)
+
+    header_list = get_header_list()
+    set_header(request_handle, header_list)
+
+    data=urllib2.urlopen(request_handle, full_url)
+    Data=data.read()
+    print Data
+
+
 def basic():
     url=""
     full_url='http://www.baidu.com'+url
@@ -33,6 +55,10 @@ def url_decode():
     decoded_data = urllib2.unquote(jsondata)
     print decoded_data
 
-#get_header_test()
-#basic()
-url_decode()
+
+if __name__ == "__main__":
+    test()
+    #get_header_test()
+    #basic()
+    #url_decode()
+
