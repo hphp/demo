7306437ba0ee544c1d22f43af2c626a651cbe9fd
hphp <hphpcarrot@gmail.com>
2014-08-04 16:00:35 +0800

    subprocess.call

---
+++ demo_python/d_subprocess.py
@@ -0,0 +1,11 @@
+
+import subprocess
+
+#subprocess.Popen('echo "helloworld"')
+subprocess.call(['sleep','3'])
+subprocess.call(['echo', '"helloworld"'])
+subprocess.call(['echo', '"helloworld2"'])
+subprocess.call(['echo', '"helloworld3"'])
+subprocess.call(['echo', '"helloworld4"'])
+
+# execute in order.
