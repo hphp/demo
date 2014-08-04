3dcbf59574fa5dc60c3bf2f7d1130d693e39cf60
hphp <hphpcarrot@gmail.com>
2014-08-04 14:32:03 +0800

    python/list files with fnmatch.filter

--- demo_python/d_list_file.py
+++ demo_python/d_list_file.py
@@ -1,7 +1,23 @@
 
 import os
-ll = os.listdir("./")
-ll = os.listdir("../")
-i = 0
-for i in range(10):
-    print ll[i] 
+import fnmatch
+
+def basic(director):
+    ll = os.listdir("./")
+    ll = os.listdir("../")
+    ll = os.listdir(director)
+    i = 0
+    for i in range(10):
+        print ll[i] 
+
+def d_filter(directory, reg):
+    ll = os.listdir(directory)
+    ll = fnmatch.filter(ll, reg)
+    for i in range(min(10, len(ll))):
+        print ll[i] 
+
+def test_basic():
+    basic("./")
+    basic("./*.py")
+
+d_filter("./","*.py")
