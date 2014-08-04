5f556336c064ef6fb1c738cb9515a4aaeda571ab
hphp <hphpcarrot@gmail.com>
2014-08-04 16:59:44 +0800

    python int to ascii, ord(unichar), unichr(int)

--- demo_python/d_convert.py
+++ demo_python/d_convert.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+def int_to_ascii():
+    i = 1
+    print unichr(i+ord('a'))
+
 def int_to_str():
     i = 1
     print str(i)
@@ -9,4 +13,6 @@ def list_str_to_int():
     results = map(int , r)
     print type(results),len(results),type(results[0])
 
-list_str_to_int()
+int_to_ascii()
+#int_to_str()
+#list_str_to_int()
