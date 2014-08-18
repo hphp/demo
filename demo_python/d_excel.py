bae7aa3de6907688c8f929a26ddb1682c4eebac7
hphp <hphpcarrot@gmail.com>
2014-08-18 09:49:37 +0800

    d_excel.py manipulate with excel 2003 (using xlrd) and 2007, using openxl

--- demo_python/d_excel.py
+++ demo_python/d_excel.py
@@ -1,10 +1,24 @@
 #encoding:utf-8
-import xlrd
-data = xlrd.open_workbook('d_excel.xls')
-table = data.sheet_by_index(0)
-row1 = table.row_values(0)
-column1 = table.col_values(0)
-cell = row1[0].encode('utf-8')
-cell = column1[-1].encode('utf-8')
-print row1, len(row1), type(row1)
-print cell, len(cell), type(cell)
+
+def for_excel_2007():
+    #pip install openpyxl
+    import openpyxl
+    wb = openpyxl.load_workbook('d_excel.xlsx')
+    # cant manipulate xls anymore.
+    #sheet_names = wb.get_sheet_names()
+    ws = wb.get_sheet_by_name("Sheet1")
+    print ws.cell('A1').value
+    # will just print the normal chinese instead of messy codes in spite of whether encoding:utf8 is set.
+
+def for_excel_2003():
+    import xlrd
+    data = xlrd.open_workbook('d_excel.xls')
+    table = data.sheet_by_index(0)
+    row1 = table.row_values(0)
+    column1 = table.col_values(0)
+    cell = row1[0].encode('utf-8')
+    cell = column1[-1].encode('utf-8')
+    print row1, len(row1), type(row1)
+    print cell, len(cell), type(cell)
+
+for_excel_2007()
